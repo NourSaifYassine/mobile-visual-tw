ddf08eca0f00011df4aaf2939a955e797ef068db
Nour Saif Yassine <nour.saif.yassine@gmail.com>
2025-03-01 15:15:45 +0100

    add: eventlistener

--- code/src/components/Navbar.jsx
+++ code/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import image from "../assets/image.png";
 import { useEffect } from "react";
 
 export default function Navbar() {
-    const onpress = useEffect(() => {
+    useEffect(() => {
         const navLinks = document.getElementById("nav-links");
         const menu = document.getElementById("menu");
 
@@ -14,15 +14,15 @@ export default function Navbar() {
         menu.addEventListener("click", onPress);
 
         return () => {
-            onPress();
             menu.removeEventListener("click", onPress);
         };
     }, []);
+
     return (
         <nav className="flex w-screen bg-sky-200 flex-col justify-center items-center pt-4 pl-5 pr-5">
             <div className="flex">
                 <h1 className="mr-[80px]">Mobile-Visual-TW</h1>
-                <img onClick={onpress} className="w-[20vw] h-[10vh]" alt="menu burger" src={image} id="menu"></img>
+                <img className="w-[20vw] h-[10vh]" alt="menu burger" src={image} id="menu"></img>
             </div>
             <ul className="flex-col bg-sky-400 h-full h-[72px] w-screen transition ease-in-out flex translate-x-0" id="nav-links">
                 <li>
