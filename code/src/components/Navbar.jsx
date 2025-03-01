ebb1fb6aaed44aa47c9201cac25756a9deb8772a
Nour Saif Yassine <nour.saif.yassine@gmail.com>
2025-03-01 15:21:46 +0100

    add: useState

--- code/src/components/Navbar.jsx
+++ code/src/components/Navbar.jsx
@@ -1,39 +1,39 @@
 import { Link } from "react-router-dom";
 import image from "../assets/image.png";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Navbar() {
-    useEffect(() => {
-        const navLinks = document.getElementById("nav-links");
-        const menu = document.getElementById("menu");
+    const [navOpen, setNavOpen] = useState(false);
 
-        const onPress = () => {
-            navLinks.classList.toggle("translate-x-[100vw]");
-        };
-
-        menu.addEventListener("click", onPress);
-
-        return () => {
-            menu.removeEventListener("click", onPress);
-        };
-    }, []);
+    const handleMenuClick = () => {
+        setNavOpen(!navOpen);
+    };
 
     return (
         <nav className="flex w-screen bg-sky-200 flex-col justify-center items-center pt-4 pl-5 pr-5">
             <div className="flex">
                 <h1 className="mr-[80px]">Mobile-Visual-TW</h1>
-                <img className="w-[20vw] h-[10vh]" alt="menu burger" src={image} id="menu"></img>
+                <img
+                    className="w-[20vw] h-[10vh]"
+                    alt="menu burger"
+                    src={image}
+                    id="menu"
+                    onClick={handleMenuClick}
+                />
             </div>
-            <ul className="flex-col bg-sky-400 h-full h-[72px] w-screen transition ease-in-out flex translate-x-0" id="nav-links">
+            <ul
+                className={`flex-col bg-sky-400 h-full h-[72px] w-screen transition ease-in-out flex ${
+                    navOpen ? "translate-x-0" : "translate-x-[100vw]"
+                }`}
+                id="nav-links"
+            >
                 <li>
                     <Link to="/">Home</Link>
                 </li>
                 <li>
                     <Link to="/coding">Coding</Link>
                 </li>
-                <li>
-                    Hello
-                </li>
+                <li>Hello</li>
             </ul>
         </nav>
     );
